f76573d31cb04969eb6b39078c5bc2bcd6843a11
Dev Jyoti Patra <devjyoti130@gmail.com>
2025-07-30 22:01:48 +0530

    117th Commit

--- 30July2025.py
+++ 30July2025.py
@@ -1,4 +1,12 @@
-class Solution:
-    def smallestSubarrays(self, a: List[int]) -> List[int]:
-        return [*map(lambda d:max(d.values())-d[-1]+1,accumulate(range(len(a)-1,-1,-1),
-            lambda d,i:d|{q:i for q in range(32) if a[i]&1<<q}|{-1:i},initial={-1:0}))][:0:-1]
+class Solution(object):
+    def longestSubarray(self, nums):
+        ans, cnt = 0, 0
+        
+        max_element = max(nums)
+        for num in nums:
+            if num == max_element:
+                cnt += 1
+            else:
+                cnt = 0
+            ans = max(ans, cnt)
+        return ans
